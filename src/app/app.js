06bc8c652dd11ba71ba12bc008471792397033f7
FenixAlive <luisangelmf@gmail.com>
2020-06-30 01:38:16 -0500

    enums, conceptos, traslados

--- src/app/app.js
+++ src/app/app.js
@@ -14,7 +14,7 @@
     }
     function rellenarCabe(id, titulo, data){
       if(data != ""){
-        document.getElementById(id).innerHTML=`<div class="datoCont"><div class="datoTit"><b>${titulo}: </b></div><div class="datoCab">${data}</div></div>`;
+        document.getElementById(id).innerHTML=`<div class="datoCont borde"><div class="datoTit"><b>${titulo}: </b></div><div class="datoCab">${data}</div></div>`;
       }
     }
     function rellenarFecha(id, titulo, data){
