249f8d154acfd4a8ccdde8ebf16d4756de798cf1
FenixAlive <luisangelmf@gmail.com>
2020-07-05 18:15:16 -0500

    cambios a css

--- src/app/app.js
+++ src/app/app.js
@@ -21,11 +21,11 @@ function showConcep(isIt){
     document.getElementById("conceptos").innerHTML=`
     <div class="concepCont" id="concepto_-1">
       <div class="concepBase cabrows borde">
-        <div class="cantidad"><b>Cantidad</b></div>
-        <div class="precioUni"><b>Precio Unitario</b></div>
-        <div class="descrip"><b>Descripción</b></div>
-        <div class="descConcep"><b>Descuento</b></div>
-        <div class="importeConcep"><b>Importe</b></div>
+        <div class="flexGrow1 width12"><b>Cantidad</b></div>
+        <div class="flexGrow1 width12"><b>Precio Unitario</b></div>
+        <div class="minWidth2 flexGrow4"><b>Descripción</b></div>
+        <div class="flexGrow1 width12"><b>Descuento</b></div>
+        <div class="flexGrow1 width12"><b>Importe</b></div>
       </div>
     </div>
     `;
@@ -37,28 +37,28 @@ function showConcep(isIt){
 function addConcep(idx, cantidad, claveUnidad, unidad, claveProdServ, numIde, valorUni, descrip, importe, descuento){
   //quitar lo que no se tiene en los adicionales
   if(unidad != ""){
-    var unidad = `<div class="unidad datoContCenter"><div class="datoTit"><b>Unidad:</b></div><div class="datoCab">${unidad}</div></div>`;
+    var unidad = `<div class="unidad cabrows"><div class="datoTit"><b>Unidad:</b></div><div class="datoCab">${unidad}</div></div>`;
   }
   if(claveUnidad != ""){
-    var claveUnidad = `<div class="claveUnidad datoContCenter"><div class="datoTit"><b>Clave de Unidad:</b></div><div class="datoCab">${claveUnidad}</div></div>`;
+    var claveUnidad = `<div class="claveUnidad cabrows"><div class="datoTit"><b>Clave de Unidad:</b></div><div class="datoCab">${claveUnidad}</div></div>`;
   }
   if(claveProdServ != ""){
-    var claveProdServ = `<div class="claveProdServ datoContCenter"><div class="datoTit"><b>Clave Producto/Servicio:</b></div><div class="datoCab"> ${claveProdServ}</div></div>`;
+    var claveProdServ = `<div class="claveProdServ cabrows"><div class="datoTit"><b>Clave Producto/Servicio:</b></div><div class="datoCab"> ${claveProdServ}</div></div>`;
   }
   if(numIde != ""){
-    var numIde = `<div class="noIdent datoContCenter"><div class="datoTit"><b>Num. Identificación:</b></div><div class="datoCab"> ${numIde}</div></div>`;
+    var numIde = `<div class="noIdent cabrows"><div class="datoTit"><b>Num. Identificación:</b></div><div class="datoCab"> ${numIde}</div></div>`;
   }
   var concep = `
   <div class="concepCont" id="concepto_${idx}">
       <div class="concepBase cabrows borde pointer" id="concepBase_${idx}">
-        <div class="cantidad">${cantidad}</div>
-        <div class="precioUni">${valorUni}</div>
-        <div class="descrip">${descrip}</div>
-        <div class="descConcep">${descuento}</div>
-        <div class="importeConcep">${importe}</div>
+        <div class="flexGrow1 width12">${cantidad}</div>
+        <div class="flexGrow1 width12">${valorUni}</div>
+        <div class="minWidth2 flexGrow4">${descrip}</div>
+        <div class="flexGrow1 width12">${descuento}</div>
+        <div class="flexGrow1 width12">${importe}</div>
       </div>
       <div class="concepDet" id="concepDet_${idx}">
-        <div class ="datosAdCol borde" id="datosAd_${idx}">
+        <div class ="cabrows datosAdCol borde" id="datosAd_${idx}">
           ${unidad}
           ${claveUnidad}
           ${claveProdServ}
@@ -86,13 +86,13 @@ function addConcep(idx, cantidad, claveUnidad, unidad, claveProdServ, numIde, va
 
 function trasRetConcepCabe(idx){
   document.getElementById(`datosAd_${idx}`).insertAdjacentHTML("afterend", `<div class="borde trasRetConcepCont" id="trasRetConcepCont_${idx}">
-                                                                              <div id="trasRetConcep_${idx}_-1" class="retConcepCont bordeDownDash datosAdCol">
-                                                                              <div class="datoContTrasRetCabe"><b>Tipo</b></div>
-                                                                              <div class="datoContTrasRetCabe"><b>Impuesto</b></div>
-                                                                              <div class="datoContTrasRetCabe"><b>Base a Aplicar Impuesto</b></div>
-                                                                              <div class="datoContTrasRetCabe"><b>Tipo de Factor</b></div>
-                                                                              <div class="datoContTrasRetCabe"><b>Tasa o Cuota</b></div>
-                                                                              <div class="datoContTrasRetCabe"><b>Importe de Impuesto</b></div>
+                                                                              <div id="trasRetConcep_${idx}_-1" class="bordeDownDash cabrows">
+                                                                                <div class="cabrowsStart width12"><b>Tipo</b></div>
+                                                                                <div class="cabrowsStart width12"><b>Impuesto</b></div>
+                                                                                <div class="cabrowsStart width12"><b>Base a Aplicar Impuesto</b></div>
+                                                                                <div class="cabrowsStart width12"><b>Tipo de Factor</b></div>
+                                                                                <div class="cabrowsStart width12"><b>Tasa o Cuota</b></div>
+                                                                                <div class="cabrowsStart width12"><b>Importe de Impuesto</b></div>
                                                                               </div>
                                                                             </div>`);
 }
@@ -108,23 +108,23 @@ function addTrasRetConcep(idx, id, tipo, importe, tasa, tipoFactor, impuesto, ba
   if (tipoFactor == "Tasa"){
     tasa = (Number(tasa)*100).toFixed(4)+"%";
   }
-  document.getElementById(`trasRetConcep_${idx}_${id-1}`).insertAdjacentHTML("afterend", `<div id="trasRetConcep_${idx}_${id}" class="retConcepCont datosAdCol">
-                                                                              <div class="datoContTrasRet"><b>${name}</b></div>
-                                                                              <div class="datoContTrasRet">${impuesto}</div>
-                                                                              <div class="datoContTrasRet">${base}</div>
-                                                                              <div class="datoContTrasRet">${tipoFactor}</div>
-                                                                              <div class="datoContTrasRet">${tasa}</div>
-                                                                              <div class="datoContTrasRet">${importe}</div>
+  document.getElementById(`trasRetConcep_${idx}_${id-1}`).insertAdjacentHTML("afterend", `<div id="trasRetConcep_${idx}_${id}" class="retConcepCont cabrows datosAdCol">
+                                                                              <div class="cabrowsStart width12"><b>${name}</b></div>
+                                                                              <div class="cabrowsStart width12">${impuesto}</div>
+                                                                              <div class="cabrowsStart width12">${base}</div>
+                                                                              <div class="cabrowsStart width12">${tipoFactor}</div>
+                                                                              <div class="cabrowsStart width12">${tasa}</div>
+                                                                              <div class="cabrowsStart width12">${importe}</div>
                                                                             </div>`);
 }
 //agrega la cabecera a la tabla de los impuestos generales
 function impuestosCabe(){
-  document.getElementById("impuestos").innerHTML = `<div class="datoCont bordeUpDash bordeDownDash impCont datosAdCol" id="impCont_-1">
-                                                      <div class="datoContImp"><b>Tipo</b></div>
-                                                      <div class="datoContImp"><b>Impuesto</b></div>
-                                                      <div class="datoContImp"><b>Factor</b></div>
-                                                      <div class="datoContImp"><b>Tasa o Cuota</b></div>
-                                                      <div class="datoContImp datoD"></div>
+  document.getElementById("impuestos").innerHTML = `<div class="cabrowsStart bordeUpDash bordeDownDash impCont cabrows datosAdCol" id="impCont_-1">
+                                                      <div class="width20"><b>Tipo</b></div>
+                                                      <div class="width20"><b>Impuesto</b></div>
+                                                      <div class="width20"><b>Factor</b></div>
+                                                      <div class="width20"><b>Tasa o Cuota</b></div>
+                                                      <div class="width20"></div>
                                                   </div>`
 }
 //agrega los traslados y retenciones dentro del nodo impuestos general
@@ -135,17 +135,17 @@ function addTrasRetImp(idx, tipo, impuesto, factor, tasa, importe){
   if (factor == "Tasa"){
     tasa = (Number(tasa)*100).toFixed(4)+"%";
   }
-  document.getElementById(`impCont_${idx-1}`).insertAdjacentHTML("afterend", `<div class="datoCont impCont datosAdCol" id="impCont_${idx}">
-                                                                                <div class="datoContImp">${tipo}</div>
-                                                                                <div class="datoContImp">${impuesto}</div>
-                                                                                <div class="datoContImp">${factor}</div>
-                                                                                <div class="datoContImp">${tasa}</div>
-                                                                                <div class="datoContImp datoD">${importe}</div>
+  document.getElementById(`impCont_${idx-1}`).insertAdjacentHTML("afterend", `<div class="cabrowsStart impCont cabrows datosAdCol" id="impCont_${idx}">
+                                                                                <div class="width20">${tipo}</div>
+                                                                                <div class="width20">${impuesto}</div>
+                                                                                <div class="width20">${factor}</div>
+                                                                                <div class="width20">${tasa}</div>
+                                                                                <div class="width20 datoD">${importe}</div>
                                                                               </div>`);
 }
 function rellenar(id, titulo, data){
   if(data != ""){
-    document.getElementById(id).innerHTML=`<div class="datoCont"><div class="datoTit"><b>${titulo}: </b></div><div class="datoD">${data}</div></div>`;
+    document.getElementById(id).innerHTML=`<div class="cabrowsStart"><div class="datoTit"><b>${titulo}: </b></div><div class="datoD">${data}</div></div>`;
   }else{
     ocultarElemento(id);
   }
@@ -153,7 +153,7 @@ function rellenar(id, titulo, data){
 
 function rellenarCabe(id, titulo, data){
   if(data != ""){
-    document.getElementById(id).innerHTML=`<div class="datoCont borde">
+    document.getElementById(id).innerHTML=`<div class="cabrowsStart borde">
                                             <div class="datoTit">
                                               <b>${titulo}: </b>
                                             </div>
@@ -189,21 +189,21 @@ function rellenar_cortado(id, titulo, data){
 
 function rellenarRelacionadosCabe(tipo){
   //crear dentro de un div de relacionados un bloque para poner los relacionados de este nodo
-  document.getElementById("relacionadosAll").innerHTML = `<div id="relacionadosCab" class="borde">
+  document.getElementById("relacionadosAll").innerHTML = `<div id="relacionadosCab" class="borde cabCols">
                                                             <div class="cabrows bordeDownDash" id="relacionadosTit">
                                                               <div class="datoTit" ><b>Documentos Relacionados:</b></div>
-                                                              <div class="datoCont" id="tipoRelacion">
+                                                              <div class="cabrowsStart" id="tipoRelacion">
                                                                 <div class="datoTit"><b>Tipo de Relación:</b></div>
                                                                 <div class="datoCab"> ${tipo}</div>
                                                               </div>
                                                             </div>
-                                                            <div class="" id="relacionadoCab"></div>
+                                                            <div class="cabCols" id="relacionadoCab"></div>
                                                           </div>`
 }
 
 function addRelacionado(id, uuid){
   if (uuid != "") {
-    var info = `<div id="relacion_${id}" class="datoCont"><div class="datoTit"><b>Comprobante Relacionado UUID:</b></div><div class="datoCab">${uuid}</div>`
+    var info = `<div id="relacion_${id}" class="cabrowsStart"><div class="datoTit"><b>Comprobante Relacionado UUID:</b></div><div class="datoCab">${uuid}</div>`
     if (id == 0){
       document.getElementById("relacionadoCab").innerHTML=info;
     }else{
@@ -214,24 +214,24 @@ function addRelacionado(id, uuid){
 
 function esValido(val){
   if(val == "Vigente"){
-    document.getElementById("validar").innerHTML=`<b class="success padText">Comprobante Valido y Vigente</b>`;
+    document.getElementById("validar").innerHTML=`<b class="success">Comprobante Valido y Vigente</b>`;
   }else if(val == "pendienteOk"){
-    document.getElementById("validar").innerHTML=`<b class="warning padText">Respuesta Incompleta del Servicio de Validación, Intentelo Mas Tarde</b>`;
+    document.getElementById("validar").innerHTML=`<b class="warning">Respuesta Incompleta del Servicio de Validación, Intentelo Mas Tarde</b>`;
   }else if(val == "pendienteNo2xx"){
-    document.getElementById("validar").innerHTML=`<b class="warning padText">Sin Respuesta del Servicio de Validación, Intentelo Mas Tarde</b>`;
+    document.getElementById("validar").innerHTML=`<b class="warning">Sin Respuesta del Servicio de Validación, Intentelo Mas Tarde</b>`;
   }else{
-    document.getElementById("validar").innerHTML=`<b class="danger padText">Respuesta de Validación: ${val}</b>`;
+    document.getElementById("validar").innerHTML=`<b class="danger">Respuesta de Validación: ${val}</b>`;
   }
 }
 
 function pagosCont(){
-  document.getElementById("pagos").innerHTML=`<div class="borde" id="pagosCont">
+  document.getElementById("pagos").innerHTML=`<div class="cabCols borde" id="pagosCont">
                                                 <div class="" id=""><b>Complemento de Pago</b></div>
                                                 <div class="cabrows bordeDownDash bordeUpDash" id="pagoCabe_-1">
-                                                  <div class="" id="fechaP"><b>Fecha de Pago</b></div>
-                                                  <div class="" id="formaPagoP"><b>Forma de Pago</b></div>
-                                                  <div class="" id="monedaP"><b>Moneda de Pago</b></div>
-                                                  <div class="" id="montoP"><b>Monto de Pago</b></div>
+                                                  <div class="width30 flexGrow1" id="fechaP"><b>Fecha de Pago</b></div>
+                                                  <div class="width30 flexGrow1" id="formaPagoP"><b>Forma de Pago</b></div>
+                                                  <div class="width20 flexGrow1" id="monedaP"><b>Moneda de Pago</b></div>
+                                                  <div class="width20 flexGrow1" id="montoP"><b>Monto de Pago</b></div>
                                                 </div>
                                               </div>`
 }
@@ -239,13 +239,13 @@ function pagosCont(){
 function pagoCabe(id, fechaP, formaPP, monedaP, montoP, otros){
   var pago = `<div class="pagoCont" id="pagoCabe_${id}">
                 <div class="cabrows borde pointer" id="pagoBase_${id}">
-                  <div class="" id="fechaP">${fechaP}</div>
-                  <div class="" id="formaPagoP">${formaPP}</div>
-                  <div class="" id="monedaP">${monedaP}</div>
-                  <div class="" id="montoP">${montoP}</div>
+                  <div class="width30 flexGrow1" id="fechaP">${fechaP}</div>
+                  <div class="width30 flexGrow1" id="formaPagoP">${formaPP}</div>
+                  <div class="width20 flexGrow1" id="monedaP">${monedaP}</div>
+                  <div class="width20 flexGrow1" id="montoP">${montoP}</div>
                 </div>
                 <div class="" id="pagoDet_${id}">
-                  <div class="pagoAd cabrows">demas datos</div>
+                  <div class="flexWrap cabrows">demas datos</div>
                   <div class="docRelCont" id="docRelCont_${id}"></div>
                 </div>
               </div>`;
